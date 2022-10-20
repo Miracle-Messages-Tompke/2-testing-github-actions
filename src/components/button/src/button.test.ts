cafbd5881ddf6b421a417d3ded6d018d93449e2a
Shawn Tompke <shawntompke@gmail.com>
2022-10-20 09:58:13 -0500

    button(test): Added button tests.

--- src/components/button/src/button.test.ts
+++ src/components/button/src/button.test.ts
@@ -5,6 +5,8 @@ global.fetch = jest.fn(() =>
   Promise.resolve({ text: jest.fn() }).catch(() => jest.fn())
 ) as jest.Mock;
 
+// changed tests
+
 describe("accessibility tests", () => {
   kinds.forEach((kind) => {
     document.body.innerHTML = `<h1>Custom element test</h1> <adc-button kind=${kind}></adc-button>`;
